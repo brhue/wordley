dfcf65b29baa0f66472e1b0bacff3c0d374d2f55
Bradley Donahue <donahue.bradley@gmail.com>
2022-01-29 17:24:21 -0500

    Refactor a bit

--- main.js
+++ main.js
@@ -6,62 +6,69 @@ let wordToGuess = words[randomIndex]
 console.log(wordToGuess)
 
 let currentGuess = 0
-let currentLetter = 0
 
 let rows = document.querySelectorAll('.row')
 
 let guess = ''
+let guesses = []
 
 buildKeyboard()
 
 document.addEventListener('keydown', (e) => {
-  if (isAlpha(e.key) && currentLetter < 5) {
-    guess = guess + e.key
-    let currentRow = rows[currentGuess]
-    let letters = currentRow.querySelectorAll('.letter-spot')
-    let letter = letters[currentLetter]
+  handleInput(e.key)
+  updateGame()
+})
 
-    let span = document.createElement('span')
-    let text = document.createTextNode(e.key)
-    span.appendChild(text)
-    letter.classList.add('pop-in')
-    letter.appendChild(span)
-    currentLetter++
-  } else if (e.key === 'Backspace') {
-    if (guess.length > 0) {
-      guess = guess.slice(0, guess.length - 1)
-      let currentRow = rows[currentGuess]
-      let letters = currentRow.querySelectorAll('.letter-spot')
-      let letter = letters[currentLetter - 1]
-      letter.innerHTML = ''
-      letter.classList.remove('pop-in')
-      currentLetter--
+function updateGame() {
+  guesses.forEach((g, i) => {
+    let row = rows[i]
+    let letters = [...row.children]
+    let status = checkGuess(g, wordToGuess)
+    letters.forEach((letter, index) => {
+      letter.textContent = g[index]
+      if (status[index] == 0) {
+        letter.classList.add('wrong', 'flip')
+      } else if (status[index] == 1) {
+        letter.classList.add('almost', 'flip')
+      } else {
+        letter.classList.add('correct', 'flip')
+      }
+    })
+    updateKeyboard(status, g)
+  })
+  let row = rows[currentGuess]
+  for (let i = 0; i < row.children.length; i++) {
+    let tile = row.children[i]
+    let letter = guess[i]
+    if (letter) {
+      tile.textContent = letter
+      tile.classList.add('pop-in')
+    } else {
+      tile.textContent = ''
+      tile.classList.remove('pop-in')
     }
-  } else if (e.key === 'Enter') {
+  }
+}
+
+function handleInput(key) {
+  if (isAlpha(key) && guess.length < 5) {
+    guess = guess + key
+  } else if (key === 'Backspace' && guess.length > 0) {
+    guess = guess.slice(0, guess.length - 1)
+  } else if (key === 'Enter') {
+    if (guess.length < 5) {
+      alert('guess too short')
+      return
+    }
     if (!(words.includes(guess) || validWords.includes(guess))) {
       alert('word not in word list')
       return
     }
-    if (guess.length === 5) {
-      let status = checkGuess(guess, wordToGuess)
-      let currentRow = rows[currentGuess]
-      let letters = currentRow.querySelectorAll('.letter-spot')
-      letters.forEach((letter, index) => {
-        if (status[index] == 0) {
-          letter.classList.add('wrong', 'flip')
-        } else if (status[index] == 1) {
-          letter.classList.add('almost', 'flip')
-        } else {
-          letter.classList.add('correct', 'flip')
-        }
-      })
-      updateKeyboard(status)
-      currentGuess++
-      currentLetter = 0
-      guess = ''
-    }
+    guesses.push(guess)
+    currentGuess++
+    guess = ''
   }
-})
+}
 
 function isAlpha(key) {
   return 'abcdefghijklmnopqrstuvwxyz'.includes(key)
@@ -102,53 +109,12 @@ function buildKeyboard() {
   keyboard.append(topRow, middleRow, bottomRow)
 
   keyboard.addEventListener('click', (e) => {
-    if (e.target.matches('.keyboard-key')) {
-      let key = e.target.textContent
-      if (isAlpha(key) && guess.length < 5) {
-        guess = guess + key
-        let currentRow = rows[currentGuess]
-        let letters = currentRow.querySelectorAll('.letter-spot')
-        let letter = letters[currentLetter]
-
-        let span = document.createElement('span')
-        let text = document.createTextNode(key)
-        span.appendChild(text)
-        letter.classList.add('pop-in')
-        letter.appendChild(span)
-        currentLetter++
-      } else if (key === 'delete' && guess.length > 0) {
-        guess = guess.slice(0, guess.length - 1)
-        let currentRow = rows[currentGuess]
-        let letters = currentRow.querySelectorAll('.letter-spot')
-        let letter = letters[currentLetter - 1]
-        letter.innerHTML = ''
-        letter.classList.remove('pop-in')
-        currentLetter--
-      } else if (key === 'enter') {
-        if (!(words.includes(guess) || validWords.includes(guess))) {
-          alert('word not in word list')
-          return
-        }
-        if (guess.length === 5) {
-          let status = checkGuess(guess, wordToGuess)
-          let currentRow = rows[currentGuess]
-          let letters = currentRow.querySelectorAll('.letter-spot')
-          letters.forEach((letter, index) => {
-            if (status[index] == 0) {
-              letter.classList.add('wrong', 'flip')
-            } else if (status[index] == 1) {
-              letter.classList.add('almost', 'flip')
-            } else {
-              letter.classList.add('correct', 'flip')
-            }
-          })
-          updateKeyboard(status)
-          currentGuess++
-          currentLetter = 0
-          guess = ''
-        }
-      }
-    }
+    if (!e.target.matches('.keyboard-key')) return
+    let key = e.target.textContent
+    if (key === 'enter') key = 'Enter'
+    if (key === 'delete') key = 'Backspace'
+    handleInput(key)
+    updateGame()
   })
 }
 
@@ -164,7 +130,7 @@ function buildKeyboardRow(keys) {
   return row
 }
 
-function updateKeyboard(status) {
+function updateKeyboard(status, guess) {
   let keys = [...document.querySelectorAll('.keyboard-key')]
   status.forEach((position, i) => {
     let key = keys.find((k) => k.textContent === guess[i])
